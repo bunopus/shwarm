cb863c584341c90fb35aaad500ecacd21db8145b
Evgeny Gusev <bunopus@gmail.com>
2018-02-26 17:35:37 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-content>
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  name: 'App'
+}
+</script>
